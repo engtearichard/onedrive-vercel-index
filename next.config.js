6e6750031f473c18203dea0978ac6ec85d9ccd3e
Teacher Richard <engtearichard@gmail.com>
2026-09-02 16:10:09 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
-  i18n,
-  reactStrictMode: true,
-  // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
-  trailingSlash: true
+  // 保留原本既有的其他設定...
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL', // 允許被其他網頁嵌入
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *", // 允許所有外部來源嵌入
+          },
+        ],
+      },
+    ]
+  },
 }
