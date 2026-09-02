474b1b2a8e4bfdd85b69e5177e3a3cb33231b074
Teacher Richard <engtearichard@gmail.com>
2026-09-02 16:18:39 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
-  // 保留原本既有的其他設定...
+  i18n,
+  reactStrictMode: true,
   async headers() {
     return [
       {
         source: '/:path*',
         headers: [
           {
-            key: 'X-Frame-Options',
-            value: 'ALLOWALL', // 允許被其他網頁嵌入
-          },
-          {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors *", // 允許所有外部來源嵌入
+            value: 'frame-ancestors *',
           },
         ],
       },
