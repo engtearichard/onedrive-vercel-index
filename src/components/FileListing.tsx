a030cb1d02bd11fc89738a84f0452021d8676656
Teacher Richard <engtearichard@gmail.com>
2026-09-03 11:03:33 +0800

    Update FileListing.tsx

--- src/components/FileListing.tsx
+++ src/components/FileListing.tsx
@@ -195,7 +195,9 @@ const FileListing: FC<{ query?: ParsedUrlQuery }> = ({ query }) => {
 
   if ('folder' in responses[0]) {
     // Expand list of API returns into flattened file data
-    const folderChildren = [].concat(...responses.map(r => r.folder.value)) as OdFolderObject['value']
+    const folderChildren = ([].concat(...responses.map(r => r.folder.value)) as OdFolderObject['value']).sort((a, b) =>
+      b.name.localeCompare(a.name)
+    )
 
     // Find README.md file to render
     const readmeFile = folderChildren.find(c => c.name.toLowerCase() === 'readme.md')
